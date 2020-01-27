71f730e05992f82a6670674d85bc470eebfb01b1
Nat Dudley <59619648+natdudley-EDA@users.noreply.github.com>
2020-01-28 10:59:57 +1300

    switch to random colors

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -7,10 +7,12 @@ class Pixel extends React.Component {
       style: {
         height: '50px',
         width: '50px',
-        backgroundColor: 'Fuchsia'
+        backgroundColor: this.randomHexColor()
       }
     }
   }
+
+  randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6,0)}`
 
   render () {
     return (
