733c1ea6f8bb0b5f4e6311d404f4abdd1bc37067
Lorenzo Fernandez <57726755+lorenzo-joseph-fernandez-19@users.noreply.github.com>
2020-01-28 10:53:01 +1300

    blue square

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pixel = () => {
+  return (
+    <div style={{
+      height: '20px',
+      width: '20px',
+      backgroundColor: 'blue'
+    }}/>
+  )
+}
+
+export default Pixel
