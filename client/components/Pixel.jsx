27d1ea50728ef0b16345e0509908fc53a2129572
Brendan Olphert <57166341+brendan-olphert@users.noreply.github.com>
2020-01-28 10:45:21 +1300

    Colour

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-const Pixel = () => (
-  <div></div>
-)
+const Pixel = () => {
+  const style = {
+    height: '50px',
+    width: '50px',
+    backgroundColor: 'Fuchsia'
+  }
+  return (
+    <div style = {style}></div>
+  )
+}
 
 export default Pixel
