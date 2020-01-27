4bb32b7a699a55c0a5b4a72d8b20e67997b99a8d
Lorenzo Fernandez <57726755+lorenzo-joseph-fernandez-19@users.noreply.github.com>
2020-01-28 11:54:50 +1300

    Added random color

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,24 @@ import Pixel from './Pixel.jsx'
 
 const App = () => {
   return (
-    <div>React development has begun!
+    <div>
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
+      <Pixel />
       <Pixel />
     </div>
   )
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 
-const Pixel = () => {
-  return (
-    <div style={{
-      height: '20px',
-      width: '20px',
-      backgroundColor: 'blue'
-    }}/>
-  )
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+class Pixel extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      style: {
+        height: '20px',
+        width: '20px',
+        backgroundColor: randomHexColor()
+      }
+    }
+  }
+
+  clickHandler = evt => {
+    this.setState({
+      backgroundColor: randomHexColor()
+    })
+  }
+
+  render () {
+    return (
+      <div style={this.state.style}/>
+      <div><button onClick={</div>
+    )
+  }
 }
 
 export default Pixel
