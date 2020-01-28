84a147ab2e60d7f5c6ae8049842ae57e505252f4
Nat Dudley <59619648+natdudley-EDA@users.noreply.github.com>
2020-01-28 14:02:08 +1300

    add weird resizing stuff

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Pixel from './Pixel'
 
-const App = () => {
-  return (
-    <>
-    {Array.from({ length: 3300 }, (v, k) => <Pixel />)}
-    </>
-  )
+class App extends React.Component {
+  render () {
+    return (
+      <>
+        {Array.from({ length: 3300 }, (v, k) => <Pixel />)}
+      </>
+    )
+  }
 }
 
 export default App
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -5,37 +5,62 @@ class Pixel extends React.Component {
     super(props)
     this.state = {
       style: {
-        height: '20px',
-        width: '20px',
-        borderRadius: '20px',
+        height: '40px',
+        width: '40px',
+        borderRadius: '40px',
         backgroundColor: this.randomHexColor()
       }
     }
   }
 
-  randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-
   clickHandler = () => {
+    let size = this.randomSize()
     this.setState({
       style: {
-        height: '20px',
-        width: '20px',
-        borderRadius: '20px',
+        height: size,
+        width: size,
+        borderRadius: size,
         backgroundColor: this.randomHexColor()
       }
     })
   }
 
   mouseEnter = () => {
+    let size = this.randomSize()
     this.setState({
       style: {
-        height: '20px',
-        width: '20px',
-        borderRadius: '20px',
+        height: size,
+        width: size,
+        borderRadius: size,
         backgroundColor: this.randomHexColor()
       }
     })
   }
+
+  componentDidMount = () => {
+    this.timerID = setInterval(
+      () => this.refresh(), 1000
+    )
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerID)
+  }
+
+  refresh = () => {
+    let size = this.randomSize()
+    this.setState({
+      style: {
+        height: '40px',
+        width: '40px',
+        borderRadius: '40px',
+        backgroundColor: this.randomHexColor()
+      }
+    })
+  }
+  
+  randomSize = () => `${Math.floor(20 + Math.random() * Math.floor(20).toString()) + 'px'}`
+  randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
   render () {
     return (
